e064abccf8e43e3e4c7d1b4a889c869d98f567c2
Mazionach <mazafesio25@gmail.com>
2024-03-24 18:22:38 -0700

    Added test database script

---
+++ setup_mysql_test.sql
@@ -0,0 +1,7 @@
+-- create database
+-- if it does not exists already
+CREATE DATABASE IF NOT EXISTS hbtn_test_db;
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+GRANT ALL PRIVILEGES ON 'hbtn_test_db.*' TO 'hbnb_test'@'localhost';
+GRANT SELECT ON 'performance_schema.*' TO 'hbnb_test'@'localhost';
+FLUSH PRIVILEGES;
